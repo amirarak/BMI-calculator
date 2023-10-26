59fcaa88a07d6aa1667b2e9624f68231d030f50c
Amira <amirarakieva@gmail.com>
2023-10-26 13:29:56 +0600

    BMI calculator

---
+++ src/main/java/com/example/bmicalculator/HelloApplication.java
@@ -0,0 +1,24 @@
+package com.example.bmicalculator;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 700, 700));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
